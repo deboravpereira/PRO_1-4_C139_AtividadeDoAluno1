b12c6f60fe305a9f9137036c7a5d59a7e9781be1
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-12-06 14:20:38 -0300

    Update predict_response.py

--- predict_response.py
+++ predict_response.py
@@ -1,1 +1,21 @@
+# Bibliotecas de pré-processamento de dados de texto
+import nltk
 
+import json
+import pickle
+import numpy as np
+import random
+
+# Biblioteca load_model
+import tensorflow
+from data_preprocessing import get_stem_words
+
+#Carregue o modelo
+model = tensorflow.keras.models.load_model('./chatbot_model.h5')
+
+# Carregue os arquivos de dados
+intents = json.loads(open('./intents.json').read())
+words = pickle.load(open('./words.pkl','rb'))
+classes = pickle.load(open('./classes.pkl','rb'))
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
